7e17b6c94605330618507fc338a1b314dff2ef62
Hazedd <hazzdd@no-replay.github.com>
2025-04-16 19:50:55 +0200

    input validation and set output paths

--- src/typerCliApp/cli_app.py
+++ src/typerCliApp/cli_app.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from typing import Annotated
 
 import typer
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
 
 
 app = typer.Typer()
@@ -10,6 +13,8 @@ state = {
     "verbose": False,
     "debug": False,
 }
+
+console = Console()
 
 
 @app.command()
@@ -23,8 +28,29 @@ def process(
     excel_input: Annotated[Path, typer.Option(help="The input excel whit items to process.")],
     out_path: Annotated[Path, typer.Option(help="Last name of person to greet.")],
 ):
-    print("tester")
-    pass
+    input_errors = []
+
+    if not imx_input.exists():
+        input_errors.append(f"[red]❌ imx_input does not exist: [bold]{imx_input}[/bold][/red]")
+    elif not imx_input.suffix == '.xml':
+        input_errors.append(f"[red]❌ imx_input is not a xml file: [bold]{imx_input}[/bold][/red]")
+
+    imx_output = imx_input.with_name(f"{imx_input.stem}-processed{imx_input.suffix}")
+
+    if not excel_input.exists():
+        input_errors.append(f"[red]❌ excel_input does not exist: [bold]{excel_input}[/bold][/red]")
+    elif not excel_input.suffix == '.xlsx' or not excel_input.suffix == '.xlsm':
+        input_errors.append(f"[red]❌ excel_input is not a excel file: [bold]{imx_input}[/bold][/red]")
+
+    excel_output = imx_input.with_name(f"{excel_input.stem}-processed{excel_input.suffix}")
+
+    if input_errors:
+        console.print(Panel("\n".join(input_errors), title="[bold red]Invalid Input[/bold red]", expand=False))
+        raise typer.Exit(code=1)
+
+
+    print("[green]✔ Inputs look good. Proceeding...[/green]")
+
 
 @app.callback()
 def main(verbose: bool = False, debug: bool = False):
@@ -32,8 +58,13 @@ def main(verbose: bool = False, debug: bool = False):
     Awesome CLI app.
     """
     if verbose:
-        print("Will write verbose output")
+        print("[blue]Verbose mode enabled[/blue]")
         state["verbose"] = True
+
+    if debug:
+        print("[blue]Debug mode enabled[/blue]")
+        state["debug"] = True
+
 
 
 if __name__ == "__main__":
