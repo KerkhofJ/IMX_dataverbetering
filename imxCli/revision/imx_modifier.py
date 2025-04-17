d88c0732f94a5cc2ca75f5ddac0749d471a787d4
Hazedd <hazzdd@no-replay.github.com>
2025-04-17 12:23:57 +0200

    guard metadata if not present we do not set and warn

--- imxCli/revision/imx_modifier.py
+++ imxCli/revision/imx_modifier.py
@@ -166,10 +166,15 @@ def set_metadata(node: _Element, set_meta_parents: bool = False):
 
 def set_metadata_node(node: _Element):
     metadata = node.find(".//{http://www.prorail.nl/IMSpoor}Metadata")
+    if not metadata:
+        logger.warning("No metadata node present, metadata not set!]")
+
     original_source = [
         item
         for item in metadata.get("source", "").split("_")
-        if not any(keyword in item.lower() for keyword in ("prorail", "measure", "dv"))
+        if not any(
+            keyword in item.lower() for keyword in ("prorail", "measure", "dv")
+        )
     ]
 
     original_source.append("ProRail")
