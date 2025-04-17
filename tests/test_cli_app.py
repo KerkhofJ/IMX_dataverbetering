e52d761ff912ff50803cb03fc652e04864bb7c31
Hazedd <hazzdd@no-replay.github.com>
2025-04-17 08:25:50 +0200

    xsd files 1.2.4 cleaned 12.0.0

--- tests/test_cli_app.py
+++ tests/test_cli_app.py
@@ -1,31 +1,79 @@
+from pathlib import Path
+
 from typer.testing import CliRunner
 
-from cliApp.cli_app import app
+from imxCli.cliApp.cliApp import app
+from utils.helpers import clear_directory
 
 runner = CliRunner()
 
 
-def test_process_command_input_not_valid():
+def test_general_help_command():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_revision_help_command():
+    result = runner.invoke(app, ["revision", "--help"])
+    assert result.exit_code == 0
+
+
+def test_revision_template_help_command():
+    result = runner.invoke(app, ["revision-template", "--help"])
+    assert result.exit_code == 0
+
+
+def test_process_command():
+
+    # clean output dir
+    clear_directory(Path("data/output"))
+
+    # valid run
     result = runner.invoke(
         app,
         [
-            "process",
-            "--imx-input", "tests/data/test.imx.xml",
-            "--excel-input", "tests/data/test.xlsx",
-            "--out-path", "tests/output/result.xlsx",
+            "revision",
+            "--imx-input", "data/O_D_003122_ERTMS_SignalingDesign.xml",
+            "--excel-input", "data/issuelist.xlsx",
+            "--out-path", "data/output",
+        ],
+    )
+    assert result.exit_code == 0
+
+    # output exist
+    result = runner.invoke(
+        app,
+        [
+            "revision",
+            "--imx-input", "data/O_D_003122_ERTMS_SignalingDesign.xml",
+            "--excel-input", "data/issuelist.xlsx",
+            "--out-path", "data/output",
+        ],
+    )
+    assert result.exit_code == 1
+
+    # imx-input not present
+    result = runner.invoke(
+        app,
+        [
+            "revision",
+            "--imx-input", "data/123.xml",
+            "--excel-input", "data/issuelist.xlsx",
+            "--out-path", "data/output",
+        ],
+    )
+    assert result.exit_code == 1
+
+    # excel-input not present
+    result = runner.invoke(
+        app,
+        [
+            "revision",
+            "--imx-input", "data/O_D_003122_ERTMS_SignalingDesign.xml",
+            "--excel-input", "data/123.xlsx",
+            "--out-path", "data/output",
         ],
     )
     assert result.exit_code == 1
 
 
-def test_process_command_output_exsist():
-    result = runner.invoke(
-        app,
-        [
-            "process",
-            "--imx-input", "tests/data/test.imx.xml",
-            "--excel-input", "tests/data/test.xlsx",
-            "--out-path", "tests/output/result.xlsx",
-        ],
-    )
-    assert result.exit_code == 1
