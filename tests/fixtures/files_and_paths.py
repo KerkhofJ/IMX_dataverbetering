e1dc432ff25368bbf8d529217b4f510489c611c7
Hazedd <hazzdd@no-replay.github.com>
2025-05-01 22:03:02 +0200

    fixtures

--- tests/fixtures/files_and_paths.py
+++ tests/fixtures/files_and_paths.py
@@ -25,7 +25,7 @@ def output_path() -> str:
 def clean_output_path(output_path: str):
     # This fixture yields the output path and ensures cleanup afterward
     yield output_path
-    clear_directory(Path(output_path))
+    # clear_directory(Path(output_path))
 
 
 @pytest.fixture(scope="module")
@@ -41,3 +41,17 @@ def imx_12_container_folder() -> str:
 @pytest.fixture(scope="module")
 def imx_single_xml_file() -> str:
     return sample_path("imx_124_single_file.xml")
+
+@pytest.fixture(scope="module")
+def diff_report() -> str:
+    return sample_path("20250430_202130-diff.xlsx")
+
+
+@pytest.fixture(scope="module")
+def diff_report_edit() -> str:
+    return sample_path("20250430_202130-diff-edit.xlsx")
+
+
+@pytest.fixture(scope="module")
+def comment_list() -> str:
+    return sample_path("comment-list.xlsx")
