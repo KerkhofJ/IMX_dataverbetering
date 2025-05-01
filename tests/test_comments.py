1ffb14bd3d646db889d08cf687f1940d1e1e1bce
Hazedd <hazzdd@no-replay.github.com>
2025-05-01 22:02:18 +0200

    test methods (no tests!)

---
+++ tests/test_comments.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from imxTools.revision.comments_extractor import extract_comments_to_new_sheet
+from revision.comments_replacer import apply_comments_from_issue_list
+
+
+def test_extract_comments_to_new_sheet(diff_report, clean_output_path):
+    extract_comments_to_new_sheet(
+        diff_report, f"{Path(clean_output_path) / 'comment-list.xlsx'}", overwrite=True
+    )
+
+
+def test_extract_comments_to_new_sheet_with_add_to_wb(diff_report, clean_output_path):
+    # todo: comment sheet seems not to be on the second sheet
+    extract_comments_to_new_sheet(
+        diff_report,
+        f"{Path(clean_output_path) / 'copied-with-comments.xlsx'}",
+        add_to_wb=True,
+        overwrite=True,
+    )
+
+
+def test_extract_comments_to_new_sheet_without_overwrite(diff_report):
+    extract_comments_to_new_sheet(
+        diff_report, add_to_wb=True, overwrite=True
+    )
+
+
+def test_extract_comments_to_new_sheet_with_existing_file(diff_report_edit, comment_list, clean_output_path):
+    apply_comments_from_issue_list(
+        issue_list_path=comment_list,
+        new_diff_path=diff_report_edit,
+        output_path= f"{Path(clean_output_path) / 'updated_diff_with_comments.xlsx'}",
+    )
