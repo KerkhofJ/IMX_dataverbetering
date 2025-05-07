6586b879e7dbbf7db476c919f46ec659f34435c9
Hazedd <hazzdd@no-replay.github.com>
2025-05-07 21:56:28 +0200

    add single imx test

--- tests/test_measure.py
+++ tests/test_measure.py
@@ -1,7 +1,11 @@
-from imxInsights import ImxContainer
+from imxInsights import ImxContainer, ImxSingleFile
 
 from imxTools.insights.measure import calculate_measurements
 
 
-def test_measure_imx_12(imx_12_container: str):
+def test_measure_imx_container(imx_12_container: str):
     calculate_measurements(ImxContainer(imx_12_container))
+
+def test_measure_imx_single_file(imx_single_xml_file: str):
+    # todo: below is typehint protocol failure, we should fix this in imxInsights
+    calculate_measurements(ImxSingleFile(imx_single_xml_file).initial_situation)
