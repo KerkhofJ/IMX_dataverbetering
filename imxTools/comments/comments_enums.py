60d1726d334cb18552d1bd9e2e4d29175ec34c59
Hazedd <hazzdd@no-replay.github.com>
2025-05-12 22:43:21 +0200

    fancy ColumnEnum

--- imxTools/comments/comments_enums.py
+++ imxTools/comments/comments_enums.py
@@ -16,13 +16,3 @@ class CommentColumns(ColumnEnum):
     comment_row = "Row number of the comment"
     comment_column = "Column number of the comment"
 
-    def __str__(self):
-        return self.name
-
-    @classmethod
-    def label_to_key(cls) -> dict[str, str]:
-        return {str(col.value): col.name for col in cls}
-
-    @classmethod
-    def key_to_label(cls) -> dict[str, str]:
-        return {col.name: str(col.value) for col in cls}
--- imxTools/utils/columnEnum.py
+++ imxTools/utils/columnEnum.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 
+#TODO: if py3.10 = eol we should consider using StrEnum
 class ColumnEnum(Enum):
 
     def __str__(self):
@@ -14,5 +15,13 @@ class ColumnEnum(Enum):
     def key_to_label(cls) -> dict[str, str]:
         return {col.name: str(col.value) for col in cls}
 
+    @classmethod
+    def value_to_member(cls) -> dict[str, 'ColumnEnum']:
+        return {str(col.value): col for col in cls}
 
-
+    @classmethod
+    def from_label(cls, label: str) -> 'ColumnEnum':
+        for member in cls:
+            if str(member.value) == label:
+                return member
+        raise ValueError(f"No column found for label: {label}")
